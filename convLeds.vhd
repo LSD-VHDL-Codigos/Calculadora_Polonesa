714fcd1c2937fabbe08eaab3d4ea2dfec96960fa
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2022-12-06 16:05:13 -0300

    a

--- convLeds.vhd
+++ convLeds.vhd
@@ -15,16 +15,16 @@ begin
   proc:process(num)
     begin
       case num is
-        when 0 => HEX <= "00000011"; -- 0
-        when 1 => HEX <= "10011111"; -- 1
-        when 2 => HEX <= "00100101"; -- 2
-        when 3 => HEX <= "00001101"; -- 3
+        when 0 => HEX <= "11000000"; -- 0 --
+        when 1 => HEX <= "11111001"; -- 1
+        when 2 => HEX <= "10100100"; -- 2
+        when 3 => HEX <= "10110000"; -- 3
         when 4 => HEX <= "10011001"; -- 4
-        when 5 => HEX <= "01001001"; -- 5
-        when 6 => HEX <= "01000001"; -- 6
-        when 7 => HEX <= "00011111"; -- 7
-        when 8 => HEX <= "00000001"; -- 8
-        when 9 => HEX <= "00001001"; -- 9
+        when 5 => HEX <= "10010010"; -- 5
+        when 6 => HEX <= "11111101"; -- 6
+        when 7 => HEX <= "11111000"; -- 7
+        when 8 => HEX <= "10000000"; -- 8
+        when 9 => HEX <= "11101111"; -- 9
         when others => HEX <= "11111111"; --DELIGADO
       end case;
     end process proc;
